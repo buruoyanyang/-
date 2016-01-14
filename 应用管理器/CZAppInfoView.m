fb957443e79c150b9e8a8256a283ccd93e8fb83e
buruoyanyang <xu18716352164@gmail.com>
2016-01-14 13:59:51 +0800

    2016/01/14
    公司 13：59 本项目完成

--- 应用管理器/CZAppInfoView.m
+++ 应用管理器/CZAppInfoView.m
@@ -35,6 +35,8 @@
     self.iconView.image = [UIImage imageNamed:appInfo.icon];
 }
 - (IBAction)downloadClick:(UIButton *)sender {
+    //当点击按钮，让viewcontroller取消和用户的交互
+    self.superview.userInteractionEnabled = NO;
     sender.enabled = NO;
     //提示正在下载
     UILabel *tipView = [[UILabel alloc] init];
@@ -48,9 +50,29 @@
     CGFloat tipX = (self.superview.frame.size.width - tipWidth) / 2;
     CGFloat tipY = (self.superview.frame.size.height - tipHeight) / 2;
     tipView.frame = CGRectMake(tipX, tipY, tipWidth, tipHeight);
+    
     tipView.backgroundColor = [UIColor grayColor];
+    tipView.textAlignment = NSTextAlignmentCenter;
     
+    //透明度
+    tipView.alpha = 0;
     
+    //圆角
+    tipView.layer.cornerRadius = 5;
+    tipView.layer.masksToBounds = YES;//剪裁超过bounds的部分，默认是NO
     
+    //动画效果
+    [UIView animateWithDuration:1.0 animations:^{
+        tipView.alpha = 0.9;
+    } completion:^(BOOL finished)
+     {
+         [UIView animateWithDuration:1.0 delay:3.0 options:UIViewAnimationOptionCurveLinear animations:^{
+             tipView.alpha = 0;
+         } completion:^(BOOL finished)
+          {
+              [tipView removeFromSuperview];
+          }];
+     }];
+    self.superview.userInteractionEnabled = YES;
 }
 @end
